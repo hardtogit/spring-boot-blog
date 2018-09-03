f2b8c46367ee3e6f1fbbf69572d3472c159a2734
xiangguo <guo.xiang@56qq.com>
2018-09-03 18:40:03 +0800

    首页编写

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,4 +1,9 @@
 define(function (require,exports,module) {
     var $=require('jquery');
-
+    //输入框事件
+    $('#searchfield').on('focus blur',function () {
+        var field=this;
+        if (field.defaultValue == field.value) field.value = '';
+        else if (field.value == '') field.value = field.defaultValue;
+    })
 });
